bfd11518ba860c3698b55fd3aaf57907bc6d78df
Tuan Duyen <62172265+toduyen@users.noreply.github.com>
2021-04-22 23:23:45 +0700

    Update base.js
    change

--- src/FirebaseConfig/base.js
+++ src/FirebaseConfig/base.js
@@ -1,13 +1,14 @@
 import Rebase from "re-base";
 import firebase from 'firebase';
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBMuNhWnFCaVvv6rgIyNmk0dnTm6a4fJCI",
-    authDomain: "getdataai.firebaseapp.com",
-    databaseURL: "https://getdataai.firebaseio.com",
-    projectId: "getdataai",
+    // you add param from firebase console
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "368706777855",
-    appId: "1:368706777855:web:230bb006041bcf82"
+    messagingSenderId: "",
+    appId: ""
 });
 const base = Rebase.createClass(firebase.database());
 export {firebaseApp};
